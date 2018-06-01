6a09b85a18d85a8579ff2650229d524f942758f4
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2018-06-01 15:46:30 -0400

    Initialize / Setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+
+class App extends Component{
+
+    render(){
+        return (
+          <div className="home-landing-page">
+            Hello
+          </div>
+        );
+    };
+
+}
+
+export default App;
