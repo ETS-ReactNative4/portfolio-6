9cd70329640998a34c06a78b9a5ef610f85a0c31
Nicholas Thanasiu <nick.thanasiu@gmail.com>
2019-01-31 20:17:49 -0500

    Add Router basename prop to process.env.PUBLIC_URL; Hopefully this resolves github pages routing issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './libs/fontawesome/fontawesome-all.min';
 
 import { MobileMenuProvider } from '@providers/mobileMenuProvider';
@@ -17,14 +17,16 @@ import './styles/main.scss';
 
 
 ReactDOM.render(
-  <Router>
+  <Router basename={process.env.PUBLIC_URL}>
     <ScreenSizeProvider>
       <MobileMenuProvider>
         <App>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/blog" component={BlogPage} />
-          <Route path="/contact" component={ContactPage} />
-          <MobileMenu />
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/blog" component={BlogPage} />
+            <Route path="/contact" component={ContactPage} />
+            <MobileMenu />
+          </Switch>
         </App>
       </MobileMenuProvider>
     </ScreenSizeProvider>
